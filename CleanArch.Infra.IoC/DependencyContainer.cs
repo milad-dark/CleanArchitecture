f2ebb008a8a8aefcbbbe1377a8c96210c1d5e37e
meghlimapour <meghlimapour@HA-94BL185>
2022-01-18 13:18:15 +0330

    Impliment IoC

---
+++ CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Infrast.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application layer
+            services.AddScoped<ICourseService, CourseService>();
+            //Infra Data Layer
+            services.AddScoped<CourseRepository, CourseRepository>();
+
+        }
+    }
+}

e5529b5e75c4ba0b5ed7da6f9fc87703339b5100
meghlimapour <meghlimapour@HA-94BL185>
2022-01-18 11:46:00 +0330

    Course Repository and GetCourses method

---
+++ CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,30 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRpository _courseRpository;
+
+        public CourseService(ICourseRpository courseRpository)
+        {
+            _courseRpository = courseRpository;
+        }
+
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseRpository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch.Infrast.Data/Repository/CourseRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infrast.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infrast.Data.Repository
+{
+    public class CourseRepository : ICourseRpository
+    {
+        private UnivercityDbContext _ctx;
+
+        public CourseRepository(UnivercityDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}

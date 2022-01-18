534b84e33c28717ce81b1d51c52ca572219806f4
meghlimapour <meghlimapour@HA-94BL185>
2022-01-18 11:35:21 +0330

    Initial Application layer

---
+++ CleanArch.Domain/Interfaces/ICourseRpository.cs
@@ -0,0 +1,10 @@
+﻿using CleanArch.Domain.Models;
+using System.Collections.Generic;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICourseRpository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}

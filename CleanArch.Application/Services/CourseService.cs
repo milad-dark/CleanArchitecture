d63ce2922ae379eebae3da0f453d555bfe369bd1
meghlimapour <meghlimapour@HA-94BL185>
2022-01-18 13:04:42 +0330

    Create Solution IoC

--- CleanArch.Application/Services/CourseService.cs
+++ CleanArch.Application/Services/CourseService.cs
@@ -1,12 +1,6 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
 using CleanArch.Domain.Interfaces;
-using CleanArch.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArch.Application.Services
 {
